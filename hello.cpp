274e01ff074f685e4d647fec54d2da53e03aa915
svang56 <svang56@mail.csuchico.edu>
2018-01-29 17:22:48 -0800

    Chances the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream>//Author:Sunny Vang
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is  " << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
